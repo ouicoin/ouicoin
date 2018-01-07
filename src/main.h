25f6d1a389d7bb158209a08772884f49bd0fd501
OuiCoin Official <info@ouicoin.io>
2018-01-08 02:53:28 +0500

    Updated main.h

--- src/main.h
+++ src/main.h
@@ -74,7 +74,7 @@ static const int64_t MIN_TX_COUNT = 0;
 /** Minimum TX value (for relaying) */
 static const int64_t MIN_TX_VALUE = 0.01 * COIN;
 /** No amount larger than this (in satoshi) is valid */
-static const int64_t MAX_SINGLE_TX = 100000000 * COIN; // 30 Million OuiCoin coins
+static const int64_t MAX_SINGLE_TX = 100000000 * COIN; // 100 Million OuiCoin MAX SUPPLY
 /** Moneyrange params */
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_SINGLE_TX); }
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
